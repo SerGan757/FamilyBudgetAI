9d1b90b89a691fab42b746b7141423953333f877
Сергей Мельник <sergey@example.com>
2026-07-28 07:46:22 +0200

    Fix Railway asyncpg URL

--- app/database/db.py
+++ app/database/db.py
@@ -11,7 +11,10 @@ class Base(DeclarativeBase):
     pass
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = (
+    os.getenv("DATABASE_URL")
+    .replace("postgresql://", "postgresql+asyncpg://", 1)
+)
 
 engine = create_async_engine(
     DATABASE_URL,
